6cf78190c582050da00b19c3d1beb3d50517a522
israel-bsi <masterisraelsilva@gmail.com>
2024-05-19 15:33:39 -0300

    Adicionado desafios de código da ultima parte do modulo de manipulacao de dados

---
+++ manipulando-dados/desafios-parte-2/guardiao-dos-atributos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static bool VerificarAtributo(string atributo, int valorMinimo, int valorMaximo, int valorAtributo)
+    {
+        if (valorAtributo >= valorMinimo && valorAtributo <= valorMaximo)
+        {
+            Console.WriteLine("O valor do atributo está dentro do intervalo especificado");
+            return true;
+        }
+        Console.WriteLine("O valor do atributo está fora do intervalo especificado");
+        return false;
+    }
+
+    static void Main(string[] args)
+    {
+        string atributo = Console.ReadLine();
+
+        int valorMinimo = int.Parse(Console.ReadLine());
+
+        int valorMaximo = int.Parse(Console.ReadLine());
+
+        int valorAtributo = int.Parse(Console.ReadLine());
+
+        VerificarAtributo(atributo, valorMinimo, valorMaximo, valorAtributo);
+    }
+}
