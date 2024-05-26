73172e1a2251a3dcfd0f71b6326c52a4b3a7e72c
israel-bsi <masterisraelsilva@gmail.com>
2024-05-26 13:19:40 -0300

    Adicionado Calculadora com Testes Unitarios

---
+++ apis/CalculadoraDio/Calculadora.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace CalculadoraDio
+{
+    public class Calculadora
+    {
+        private readonly List<string> _historico;
+        private readonly string data;
+        public Calculadora(string data)
+        { 
+            this.data = data;
+            _historico = new List<string>();
+        }
+        public int Somar(int val1, int val2)
+        {
+            var res = val1 + val2;
+            _historico.Insert(0, $"Res: {res} - {data}");
+            return res;
+        }
+        public int Subtrair(int val1, int val2)
+        {
+            var res = val1 - val2;
+            _historico.Insert(0, $"Res: {res} - {data}");
+            return res;
+        }
+        public int Multiplicar(int val1, int val2)
+        {
+            var res = val1 * val2;
+            _historico.Insert(0, $"Res: {res} - {data}");
+            return res;
+        }
+        public int Dividir(int val1, int val2)
+        {
+            var res = val1 / val2;
+            _historico.Insert(0, $"Res: {res} - {data}");
+            return res;
+        }
+        public List<string> Historico()
+        {
+            _historico.RemoveRange(3, _historico.Count - 3);
+            return _historico;
+        }
+    }
+}
